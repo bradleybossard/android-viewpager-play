f8b1e65a0beeb296c24817690098505b3b98c71d
Bradley Bossard <bradleybossard@gmail.com>
2014-10-29 14:14:04 -0700

    Added button, buttons string and hanlder to play sound

--- app/src/main/java/com/bradleybossard/viewpagerexample/MainActivity.java
+++ app/src/main/java/com/bradleybossard/viewpagerexample/MainActivity.java
@@ -1,13 +1,15 @@
 package com.bradleybossard.viewpagerexample;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+/*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,5 +37,28 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+*/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button one = (Button)this.findViewById(R.id.button1);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.water_drop);
+        one.setOnClickListener(new OnClickListener(){
+
+            public void onClick(View v) {
+                mp.start();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
